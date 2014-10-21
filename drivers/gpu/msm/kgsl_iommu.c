84d20d21e80b69dd52404765d913bd3767d77f83
Chet Kener <Cl3Kener@gmail.com>
2014-10-21 18:41:36 -0400

    gcc5: gpu: msm: fix 5.0 compile
    * These need to be static to avoid these errors:
    
    drivers/built-in.o: In function `.LANCHOR1':
    msm_iommu_sec.c:(.data+0x9298): undefined reference to `kgsl_iommu_sync_lock'
    msm_iommu_sec.c:(.data+0x929c): undefined reference to `kgsl_iommu_sync_unlock'
    Makefile:877: recipe for target '.tmp_vmlinux1' failed
    make: *** [.tmp_vmlinux1] Error 1
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- drivers/gpu/msm/kgsl_iommu.c
+++ drivers/gpu/msm/kgsl_iommu.c
@@ -1021,7 +1021,7 @@ static int kgsl_iommu_init_sync_lock(struct kgsl_mmu *mmu)
  *
  * Return - int - number of commands.
  */
-inline unsigned int kgsl_iommu_sync_lock(struct kgsl_mmu *mmu,
+static inline unsigned int kgsl_iommu_sync_lock(struct kgsl_mmu *mmu,
 						unsigned int *cmds)
 {
 	struct kgsl_device *device = mmu->device;
@@ -1091,7 +1091,7 @@ inline unsigned int kgsl_iommu_sync_lock(struct kgsl_mmu *mmu,
  *
  * Return - int - number of commands.
  */
-inline unsigned int kgsl_iommu_sync_unlock(struct kgsl_mmu *mmu,
+static inline unsigned int kgsl_iommu_sync_unlock(struct kgsl_mmu *mmu,
 					unsigned int *cmds)
 {
 	struct kgsl_device *device = mmu->device;
