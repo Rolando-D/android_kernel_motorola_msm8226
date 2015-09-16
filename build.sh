10d5c3b81a5636c44d951bb5af451b36c6f5da74
RolanDroid <rolandosimoncelli8@gmail.com>
2015-09-16 21:09:15 +0200

    BUILD.SH: Add support for gcc5.1
    Signed-off-by: RolanDroid <rolandosimoncelli8@gmail.com>

--- build.sh
+++ build.sh
@@ -81,13 +81,15 @@ maintoolchain() {
 if [ -d ../android_prebuilt_toolchains ]; then
 	echo "1) 4.7 Google GCC"
 	echo "2) 4.8 Google GCC"
-	echo "3) 4.9.3 Linaro GCC"
+	echo "3) 5.1 Ubert GCC"
+	echo "4) 4.9.3 Linaro GCC"
 	unset errortoolchain
 	read -p "Choice: " -n 1 -s toolchain
 	case "$toolchain" in
 		1 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-eabi-4.7/bin/arm-eabi-"; ToolchainCompile="GCC 4.7";;
 		2 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-eabi-4.8/bin/arm-eabi-"; ToolchainCompile="GCC 4.8";;
-		3 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-cortex-linux-gnueabi-linaro_4.9.3-2015.03/bin/arm-cortex-linux-gnueabi-"; ToolchainCompile="Linaro 4.9.3";;
+		3 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-eabi-5.1/bin/arm-eabi-"; ToolchainCompile="GCC 5.1";;
+		4 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-cortex-linux-gnueabi-linaro_4.9.3-2015.03/bin/arm-cortex-linux-gnueabi-"; ToolchainCompile="Linaro 4.9.3";;
 		* ) echo "$toolchain - This option is not valid"; sleep .5; errortoolchain="ON";;
 	esac
 	if ! [ "$errortoolchain" == "ON" ]; then
