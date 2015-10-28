1034f2e6c46ab98b60309ff904e7ca9838778bb1
RolanDroid <rolandosimoncelli8@gmail.com>
2015-10-28 21:38:55 +0100

    Added build-zip.sh
    Signed-off-by: RolanDroid <rolandosimoncelli8@gmail.com>

---
+++ build-zip.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cleanzip() {
+rm -rf zip-creator/*.zip zip-creator/zImage zip-creator/system/lib/modules/*.ko
+}
+
+mkdir -p zip-creator/system/lib/modules
+cp arch/arm/boot/zImage zip-creator/kernel
+find . -name *.ko | xargs cp -a --target-directory=zip-creator/system/lib/modules/ &> /dev/null
+
+customkernel=MonsterKernel
+version=V10
+device=TheaTitan
+daytime=$(date +%d""%m""%Y)
+zipfile="$customkernel-$version-$device-$daytime.zip"
+
+cd zip-creator
+zip -r $zipfile * -x */.gitignore *.zip &> /dev/null
+cd ..
