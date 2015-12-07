6dcaa9bb546f8e9419fcc4a135fa6f1935bba73d
RolanDroid <rolandosimoncelli8@gmail.com>
2015-12-07 15:45:49 +0100

    v4
    Signed-off-by: RolanDroid <rolandosimoncelli8@gmail.com>

--- build.sh
+++ build.sh
@@ -304,7 +304,7 @@ esac
 
 if [ -e build.sh ]; then
 	customkernel=MonsterKernel
-        version=V3
+        version=V4
         device=TheaTitan
 	export ARCH=arm
 	daytime=$(date +%d""%m""%Y)
--- changelog.sh
+++ changelog.sh
@@ -11,12 +11,6 @@ bldgrn=${txtbld}$(tput setaf 2) #  green
 bldblu=${txtbld}$(tput setaf 4) #  blue
 txtrst=$(tput sgr0)
 
-echo "${bldgrn}this is an open source script, feel free to use and share it${txtbld}"
-
-echo "${bldblu}Info of MonsterKernel...${txtbld}"
-
-echo "${bldgrn}Version=${txtbld}"V3
-
 echo "${bldblu}Changelog of MonsterKernel...${txtbld}"\
 
 
